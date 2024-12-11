2aab9e24aedead095f4a10818e9b1244a4706e3a
Sven Ole Spindler <s.spindler@gmx.de>
2024-12-11 16:20:42 +0100

    establish two route groupes for content & marketing and adjust layout

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,14 @@
-import "./globals.css";
-import Header from "@/components/main-header/header";
-
+import './globals.css';
+ 
 export const metadata = {
-  title: "Next.js Page Routing & Rendering",
-  description: "Learn how to route to different pages.",
+  title: 'Next.js Page Routing & Rendering',
+  description: 'Learn how to route to different pages.',
 };
-
+ 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <div id="page">
-        <Header />
-        {children}
-        </div>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
