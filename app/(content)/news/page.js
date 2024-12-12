64f20490b50c4f73fcfc46a2b4221e36401d700d
Sven Ole Spindler <s.spindler@gmx.de>
2024-12-12 14:39:16 +0100

    fetch data directly in component on server site

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,51 +1,20 @@
-'use client';
-
 import NewsList from "@/components/news/news-list";
-import { useEffect } from "react";
-import { useState } from "react";
+// import { useEffect, useState } from "react";
 // import { DUMMY_NEWS } from "@/dummy-news";
 
-export default function NewsPage() {
-  const [isLoading, setIsLoading] = useState();
-  const [error, setError] = useState();
-  const [news, setNews] = useState();
-
-  useEffect(() => {
-    async function fetchNews() {
-      setIsLoading(true);
-      const response = await fetch("http://localhost:8080/news");
-
-      if (!response.ok) {
-        setError("Failed to fetch news.");
-        setIsLoading(false);
-      }
-
-      const news = await response.json();
-      setIsLoading(false);
-      setNews(news);
-    }
-
-    fetchNews();    
-  }, []);
-
-  if (isLoading) {
-    return <p>Loading...</p>;
+export default async function NewsPage() {
+  const response = await fetch("http://localhost:8080/news");
+  
+  if(!response.ok) {
+    return <div>Failed to fetch news</div>;
   }
-
-  if (error) {
-    return <p>{error}</p>;
-  }
-
-  let newsContent;
-
-  if (news) {
-    newsContent = <NewsList news={news} />;
-  }
+  
+  const news = await response.json();
 
   return (
     <div>
       <h1>News Page</h1>
-      {newsContent}
+      <NewsList news={news} />
     </div>
   );
 }
